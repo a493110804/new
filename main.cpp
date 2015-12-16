abf252975831c27a988959351ef4b98eca1925a6
a493110804 <493110804@qq.com>
2015-12-16 19:12:09 +0800

    动态分配储存空间
    之前是用malloc分配的动态储存空间。这次是用c++的new语句，动态分配了储存空间。最后用delet语句将动态分配的存储空间回收。

---
+++ main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  new
+//
+//  Created by 魏新宇 on 15/12/16.
+//  Copyright © 2015年 魏新宇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int *a;
+    int i,j,temp;
+    int N;
+    scanf("%d",&N);
+    a=new int[N];
+    for(i=0;i<N;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<N-1;i++)
+    {
+        for(j=0;j<N-1-j;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for (i=0;i<N;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    delete a;
+    return 0;
+}
